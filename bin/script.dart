742629c8b45b02e21a2bca8ab844359052a35b64
Hasan M. Hallak <hasanm.hallak@gmail.com>
2023-07-03 21:42:16 +0300

    finish

--- bin/script.dart
+++ bin/script.dart
@@ -1,5 +1,17 @@
-import 'package:script/script.dart' as script;
+import 'dart:async';
+import 'dart:io';
+import 'package:script/login.dart' as script;
+import 'package:script/send_passport_request.dart';
 
 void main(List<String> arguments) async {
-  await script.login();
+  await start(int.parse(arguments.first));
+}
+
+Future<void> start(int sleepInMilliseconds) async {
+  final cookies = await script.login();
+  if (cookies == null) return start(sleepInMilliseconds);
+  final isSuccess = await sendPassportRequest(cookies);
+  if (isSuccess) exit(0);
+  Timer(Duration(milliseconds: sleepInMilliseconds),
+      () => start(sleepInMilliseconds));
 }
---
+++ lib/send_passport_request.dart
@@ -0,0 +1,92 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+Future<bool> sendPassportRequest(String cookies) async {
+  print('prepearing login payload..');
+  final passportPayload = jsonEncode(
+    {
+      "ALIAS": "OPHRUHvKso",
+      "P_USERNAME": "WebSite",
+      "P_ZCITYID": 16,
+      "P_ZTOWNID": 17107755,
+      "P_ZSIDEID": null,
+      "P_ZPROCESSID": 18123129,
+      "P_FNAME": "محمد حسام",
+      "P_LNAME": "حلاق",
+      "P_FATHER": "محمود",
+      "P_MOTHER": "حلا",
+      "P_COPIES": 1,
+      "P_SYRIAID": "07020067923",
+      "P_BIRTH_PLACE": "سراقب",
+      "P_BIRTH_DATE": "1999-05-18",
+      "P_REGISTNO": "840",
+      "P_IDDOCNO": "",
+      "P_CENTERID": 60739316,
+      "P_MOBILE": "0965225683",
+      "P_TEL": "0965225683",
+      "P_ADDR_GENERAL": null,
+      "P_D_METHOD": "1",
+      "P_D_ADDRESS": null,
+      "P_ADD_ZCITYID": 16,
+      "P_ADD_ZTOWNID": 17107755,
+      "P_ADD_AREA": "",
+      "P_ADD_NAH": "",
+      "P_ADD_VILLAGE": "",
+      "P_ADD_POST_OFFICE": null,
+      "P_LEGALID": null,
+      "P_PASSPORT_TYPE": "3",
+      "P_MATE_COUNT": 0,
+      "P_PASSPORT_KIND": "10"
+    },
+  );
+
+  print('done!');
+  print(passportPayload);
+
+  print('prepearing login header..');
+  final reqHeaders = {
+    'Accept': 'application/json, text/plain, */*',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Alias': 'OPHRUHvKso',
+    'Connection': 'keep-alive',
+    'Content-Length': '670',
+    'Content-Type': 'application/json',
+    'Cookie': cookies.trim(),
+    'Host': 'api.ecscsy.com:8080',
+    'Origin': 'https://ecsc.gov.sy',
+    'Referer': 'https://ecsc.gov.sy/',
+    'Source': 'WEB',
+    'User-Agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.67',
+    'dnt': '1',
+    'sec-ch-ua':
+        '"Not.A/Brand";v="8", "Chromium";v="114", "Microsoft Edge";v="114"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-gpc': '1',
+  };
+  ;
+
+  print('done!');
+  print('sending request..');
+  try {
+    final res = await http.post(
+      Uri.parse('https://api.ecscsy.com:8080/dbm/db/execute'),
+      headers: reqHeaders,
+      body: passportPayload,
+    );
+
+    if (res.statusCode == 200) {
+      print(res.body);
+      return true;
+    } else {
+      print('${res.statusCode}: ${res.reasonPhrase}');
+      print(res.body);
+      return false;
+    }
+  } catch (e) {
+    print(e);
+    return false;
+  }
+}
