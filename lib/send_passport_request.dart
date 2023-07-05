b50a2f48104a74abf3a9ce0faea79f54b96440f5
Hasan M. Hallak <hasanm.hallak@gmail.com>
2023-07-05 22:01:12 +0300

    s

--- lib/send_passport_request.dart
+++ lib/send_passport_request.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'dart:io';
 import 'package:http/http.dart' as http;
 
 Future<bool> sendPassportRequest(String cookies) async {
@@ -42,13 +41,28 @@ Future<bool> sendPassportRequest(String cookies) async {
   print('prepearing header..');
   final reqHeaders = {
     'Accept': 'application/json, text/plain, */*',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'en-US,en;q=0.9',
     'Alias': 'OPHRUHvKso',
+    'Connection': 'keep-alive',
+    'Content-Length': '670',
     'Content-Type': 'application/json',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'cross-site',
     'Cookie': cookies.trim(),
     'Host': 'api.ecscsy.com:8080',
     'Origin': 'https://ecsc.gov.sy',
     'Referer': 'https://ecsc.gov.sy/',
     'Source': 'WEB',
+    'User-Agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.67',
+    'dnt': '1',
+    'sec-ch-ua':
+        '"Not.A/Brand";v="8", "Chromium";v="114", "Microsoft Edge";v="114"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-gpc': '1',
   };
 
   print('sending request..');
@@ -64,7 +78,7 @@ Future<bool> sendPassportRequest(String cookies) async {
       return true;
     } else {
       print('${res.statusCode}: ${res.reasonPhrase}');
-      print(jsonDecode(res.body));
+      print(utf8.decode(res.body.codeUnits));
       return false;
     }
   } catch (e) {
